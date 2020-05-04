624e0659c043d3e82597489f618216fa524ee8ea
Amanda <ansc@cin.ufpe.br>
2020-05-04 19:21:10 -0300

    uploading day12

---
+++ exercises/day12.py
@@ -0,0 +1,32 @@
+
+def add(x, y):
+    return(x+y)
+
+def subtract(x, y):
+    return(x-y)
+
+def divide(x, y):
+    if y != 0:
+        return(x/y)
+
+def multiply(x, y):
+    return(x*y)
+
+def print_show_info(tv_show):
+    for serie in tv_show:
+        title = serie["title"]
+        year = serie ["initial_release"]
+        seasons = serie["seasons"]
+        print(f"{title} ({year}) - {seasons} seasons")
+
+
+series = [
+ 	{"title": "Breaking Bad", "seasons": 5, "initial_release": 2008},
+ 	{"title": "Fargo", "seasons": 4, "initial_release": 2014},
+ 	{"title": "Firefly", "seasons": 1, "initial_release": 2002},
+ 	{"title": "Rick and Morty", "seasons": 4, "initial_release": 2013},
+ 	{"title": "True Detective", "seasons": 3, "initial_release": 2014},
+ 	{"title": "Westworld", "seasons": 3, "initial_release": 2016},
+ ]
+print_show_info(series)
+
